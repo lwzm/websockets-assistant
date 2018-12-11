268bf536e4a60579866d7d305f4a2b187755517a
q <lwzm@qq.com>
2018-12-11 14:27:38 +0800

    print with colors

--- websockets_assistant.py
+++ websockets_assistant.py
@@ -6,9 +6,41 @@ from datetime import datetime
 
 import websockets
 
+"""
+find corlord fonts from colorama:
 
-def log(*args):
-    print(datetime.now(), *args, file=sys.stderr, flush=True)
+    colors = {}
+    for i in dir(colorama.Fore):
+        if not i.startswith("_"):
+            print(getattr(colorama.Fore, i), i)
+            colors[i.lower()] = getattr(colorama.Fore, i)
+"""
+
+colors = {
+    'black': '\x1b[30m',
+    'blue': '\x1b[34m',
+    'cyan': '\x1b[36m',
+    'green': '\x1b[32m',
+    'lightblack_ex': '\x1b[90m',
+    'lightblue_ex': '\x1b[94m',
+    'lightcyan_ex': '\x1b[96m',
+    'lightgreen_ex': '\x1b[92m',
+    'lightmagenta_ex': '\x1b[95m',
+    'lightred_ex': '\x1b[91m',
+    'lightwhite_ex': '\x1b[97m',
+    'lightyellow_ex': '\x1b[93m',
+    'magenta': '\x1b[35m',
+    'red': '\x1b[31m',
+    'reset': '\x1b[39m',
+    'white': '\x1b[37m',
+    'yellow': '\x1b[33m',
+}
+
+
+def log(*args, color=None):
+    color = colors.get(color, "")
+    ts = datetime.now()
+    print(f"{color}{ts}", *args, colors["reset"], file=sys.stderr, flush=True)
 
 
 async def _loop(uri, consume=lambda x: x, assist=None, once=False):
@@ -18,15 +50,15 @@ async def _loop(uri, consume=lambda x: x, assist=None, once=False):
         ts = datetime.now()
         try:
             async with websockets.connect(uri) as ws:
-                log(ws, name)
+                log(f"{id(ws):x}", name, color="green")
                 if assist:
                     asyncio.get_event_loop().create_task(assist(ws))
                 async for o in ws:
                     consume(o)
         except Exception as e:
-            log(type(e), e)
+            log(type(e), e, color="red")
         finally:
-            log(ws, name, datetime.now() - ts)
+            log(f"{id(ws):x}", name, datetime.now() - ts, color="yellow")
 
         if once:
             break
